6e4734dc47cf4a923400d8e481c1e07d626d1e2f
RodrigoSHL <shl.catalan@gmail.com>
2023-05-11 10:14:48 -0400

    FEAT: finish results components
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

---
+++ src/pages/ResultsPage/ResultsComponent.tsx
@@ -0,0 +1,73 @@
+import { Box, Grid, Typography } from "@mui/material";
+import React from "react";
+import styles from "./ResultPage.module.css";
+
+interface Team {
+  name: string;
+  shield: string;
+}
+
+interface Results {
+  category: string;
+  localTeam: Team;
+  visitTeam: Team;
+  localTeamScore: number;
+  visitTeamScore: number;
+  dateGame: string;
+}
+
+interface Props {
+  championshipDate: string;
+  results: Results[];
+}
+
+const ResultsComponent = ({ championshipDate, results }: Props) => {
+  return (
+    <>
+      <Grid item xs={12} sx={{ padding: 2 }}>
+        <Typography align="center" variant="h5">
+          {"Fecha"} - {championshipDate}
+        </Typography>
+        <br />
+      </Grid>
+      {results.map((result, key) => (
+        <Grid key={key} item xs={12} md={6} lg={3} sx={{ padding: 2 }}>
+          <Box sx={{ padding: 2, backgroundColor: "blue" }}>
+            <Typography align="center" color="white" variant="subtitle1">
+              {result.category.toUpperCase()}
+            </Typography>{" "}
+            <Box className={styles.container_results}>
+              <Box>
+                <img
+                  src={result.localTeam.shield}
+                  alt={`${result.localTeam.name}shield`}
+                  className={styles.shield}
+                />
+                <Typography align="center" color="white" variant="subtitle1">
+                  {result.localTeam.name}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography align="center" color="white" variant="subtitle1">
+                  {result.localTeamScore} - {result.visitTeamScore}
+                </Typography>
+              </Box>
+              <Box>
+                <img
+                  src={result.visitTeam.shield}
+                  alt={`${result.visitTeam.name}shield`}
+                  className={styles.shield}
+                />
+                <Typography align="center" color="white" variant="subtitle1">
+                  {result.visitTeam.name}
+                </Typography>
+              </Box>{" "}
+            </Box>
+          </Box>
+        </Grid>
+      ))}
+    </>
+  );
+};
+
+export default ResultsComponent;
--- src/pages/ResultsPage/ResultsPage.tsx
+++ src/pages/ResultsPage/ResultsPage.tsx
@@ -2,9 +2,10 @@ import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
 import { Orompello, VillaBerlin } from "./data";
 import styles from "./ResultPage.module.css";
+import ResultsComponent from "./ResultsComponent";
 const championshipDate = [
   {
-    championshipDate: 1,
+    championshipDate: "1",
     dateCreation: "23-23-2023",
     results: [
       {
@@ -50,7 +51,7 @@ const championshipDate = [
     ],
   },
   {
-    championshipDate: 2,
+    championshipDate: "2",
     dateCreation: "23-23-2023",
     results: [
       {
@@ -72,56 +73,13 @@ const ResultsPage = () => {
         <b>RESULTADOS CAMPEONATO OFICIAL {new Date().getFullYear()}</b>
       </Typography>
       <Grid item xs={12} className={styles.container_results}>
-        {championshipDate.map((date, key) =>
-          date.results.map((result, key) => (
-            <Grid key={key} item xs={12} md={6} lg={3} sx={{ padding: 2 }}>
-              <Box sx={{ padding: 2, backgroundColor: "blue" }}>
-                <Typography align="center" color="white" variant="subtitle1">
-                  {result.category.toUpperCase()}
-                </Typography>{" "}
-                <Box className={styles.container_results}>
-                  <Box>
-                    <img
-                      src={result.localTeam.shield}
-                      alt={`${result.localTeam.name}shield`}
-                      className={styles.shield}
-                    />
-                    <Typography
-                      align="center"
-                      color="white"
-                      variant="subtitle1"
-                    >
-                      {result.localTeam.name}
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography
-                      align="center"
-                      color="white"
-                      variant="subtitle1"
-                    >
-                      {result.localTeamScore} - {result.visitTeamScore}
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <img
-                      src={result.visitTeam.shield}
-                      alt={`${result.visitTeam.name}shield`}
-                      className={styles.shield}
-                    />
-                    <Typography
-                      align="center"
-                      color="white"
-                      variant="subtitle1"
-                    >
-                      {result.visitTeam.name}
-                    </Typography>
-                  </Box>{" "}
-                </Box>
-              </Box>
-            </Grid>
-          ))
-        )}
+        {championshipDate.map((date, key) => (
+          <ResultsComponent
+            key={key}
+            championshipDate={date.championshipDate}
+            results={date.results}
+          />
+        ))}
       </Grid>
     </Grid>
   );
